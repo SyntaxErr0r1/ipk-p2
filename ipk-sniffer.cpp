1ba5797765ed6c6b6505307d4d2e76db0174f4a2
Juraj Dedic <jurajdedic@gmail.com>
2022-04-22 20:30:33 +0200

    Added IPv6 support

--- ipk-sniffer.cpp
+++ ipk-sniffer.cpp
@@ -52,6 +52,87 @@ void print_ipv4(uint32_t ip){
     printf("%03d.%03d.%03d.%03d",(ip & 0xFF000000) >> 24,(ip & 0x00FF0000) >> 16,(ip & 0x0000FF00) >> 8,ip & 0x000000FF);
 }
 
+void find_zeros(struct in6_addr* address, int* zeros_start,int* zeros_length){
+    int longest_zero_start = -1;
+    int longest_zero_len = -1;
+    int current_zero_start = -1;
+    bool previous_zero = false;
+    for (int i = 0; i < 8; i++)
+    {
+        // fprintf(stderr,"%i ",i);
+        int n0 = (int)address->s6_addr[2*i];
+        int n1 = (int)address->s6_addr[2*i+1];
+        if(n0 == 0 && n1 == 0){
+            if(previous_zero){
+                if(current_zero_start == -1){
+                    current_zero_start = i-1;
+                    // fprintf(stderr,"new current start at %i\n",current_zero_start);
+                }
+                int current_len = i - current_zero_start;
+                if(current_len > longest_zero_len){
+                    longest_zero_start = current_zero_start;
+                    longest_zero_len = current_len;
+                }
+            }
+        }else{
+            current_zero_start = -1;
+            // fprintf(stderr,"current zero reset at %i\n",i);
+        }
+
+        previous_zero = n0 == 0 && n1 == 0;
+    }
+    (*zeros_start) = longest_zero_start;
+    (*zeros_length) = longest_zero_len;
+}
+
+void print_pair(int n0, int n1){
+    if(n0 == 0 && n1 == 0)
+        putchar('0');
+    else{
+        if(n0 == 0)
+            printf("%x",n1);
+        printf("%x%02x",n0,n1);
+
+    }
+}
+
+/**
+ * @brief prints IPv6 address from numeric format
+ * @param address the ipv6 to print
+ * @note according to RFC5952
+ */
+void print_ipv6(struct in6_addr* address){
+    int zeros_start = 0;
+    int zeros_length = 0;
+
+    find_zeros(address,&zeros_start,&zeros_length);
+    // fprintf(stderr,"\nlongest zero start: %d, len: %d\n",zeros_start,zeros_length);
+    
+    for (int i = 0; i < 8; i++)
+    {
+        int n0 = (int)address->s6_addr[2*i];
+        int n1 = (int)address->s6_addr[2*i+1];
+        if(i >= zeros_start && zeros_length >= 0){
+            //if there are zeros we will skip
+            if(i == zeros_start)
+                printf(":");
+            zeros_length--;
+        }else{
+            //otherwise print normally
+            print_pair(n0,n1);
+
+            if(i < 7)
+                putchar(':');
+        }
+    }
+    
+    // printf("%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x",
+    // (int)address->s6_addr[0], (int)address->s6_addr[1],(int)address->s6_addr[2], (int)address->s6_addr[3],
+    // (int)address->s6_addr[4], (int)address->s6_addr[5],(int)address->s6_addr[6], (int)address->s6_addr[7],
+    // (int)address->s6_addr[8], (int)address->s6_addr[9],(int)address->s6_addr[10], (int)address->s6_addr[11],
+    // (int)address->s6_addr[12], (int)address->s6_addr[13],(int)address->s6_addr[14], (int)address->s6_addr[15]);
+}
+
 /**
  * @brief Prints ethernet header of given frame
  * @param packet the packet of which header will be printed
@@ -85,6 +166,19 @@ void print_ip_header(const u_char* packet){
 
     printf("src IP: %s", inet_ntoa(addr_src.sin_addr));
     printf("\ndst IP: %s",inet_ntoa(addr_dst.sin_addr));
+    putchar('\n');
+}
+
+/**
+ * @brief Prints header of IPv6 packet
+ * @param packet the packet to print
+ */
+void print_ip6_header(const u_char* packet){
+    struct ip6_hdr* ip6_header = (struct ip6_hdr *) (packet + ETH_HLEN);
+    printf("src IP:");
+    print_ipv6(&(ip6_header->ip6_src));
+    printf("\ndst IP:");
+    print_ipv6(&(ip6_header->ip6_dst));
     putchar('\n');
 }
 
@@ -151,12 +245,16 @@ bool has_port(const u_char* packet, uint16_t port){
  * @param offset where to start printing from
  */
 void print_payload_line(const u_char* packet, int len, int offset){
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < 16; i++)
     {
         unsigned char c = (unsigned char) packet[i+offset];
         if(i == 8)
             putchar(' ');
-        printf("%02x",c);
+        if(i < len)
+            printf("%02x",c);
+        else
+            printf("  ");
+
         putchar(' ');
     }
     
@@ -200,16 +298,23 @@ void print_payload(const u_char* packet, int len){
  * @param packet will be printed
  * @param packet_header header of the packet
  */
-void print_tcp(const u_char* packet, pcap_pkthdr packet_header){
-    struct iphdr* ip_header = (struct iphdr *) (packet + ETH_HLEN);
-    int ip_header_len = (int) ip_header->ihl*4;
-    print_ip_header(packet);
-    fprintf(stderr,"IP header length: %d\n",ip_header_len);
+void print_tcp(const u_char* packet, pcap_pkthdr packet_header,bool is_ipv6){
+    int ip_header_len;
+    if(!is_ipv6){
+        struct iphdr* ip_header = (struct iphdr *) (packet + ETH_HLEN);
+        ip_header_len = (int) ip_header->ihl*4;
+        print_ip_header(packet);
+    }else{
+        ip_header_len = 40;
+        print_ip6_header(packet);
+    }
 
     fprintf(stderr,"protocol: TCP\n");
     struct tcphdr* tcp_header= (struct tcphdr*)(packet + ip_header_len + ETH_HLEN);
     printf("src port: %u\n",ntohs(tcp_header->source));
     printf("dst port: %u\n",ntohs(tcp_header->dest));
+
+    print_payload(packet,packet_header.len);
 }
 
 /**
@@ -218,10 +323,16 @@ void print_tcp(const u_char* packet, pcap_pkthdr packet_header){
  * @param packet the packet to be printed
  * @param packet_header header of the packet
  */
-void print_udp(const u_char* packet, pcap_pkthdr packet_header){
-    struct iphdr* ip_header = (struct iphdr *) (packet + ETH_HLEN);
-    int ip_header_len = ip_header->ihl*4;
-    print_ip_header(packet);
+void print_udp(const u_char* packet, pcap_pkthdr packet_header, bool is_ipv6){
+    int ip_header_len;
+    if(!is_ipv6){
+        struct iphdr* ip_header = (struct iphdr *) (packet + ETH_HLEN);
+        ip_header_len = (int) ip_header->ihl*4;
+        print_ip_header(packet);
+    }else{
+        ip_header_len = 40;
+        print_ip6_header(packet);
+    }
 
     fprintf(stderr,"protocol: UDP\n");
     struct udphdr* udp_header = (struct udphdr*)(packet + ip_header_len + ETH_HLEN);
@@ -239,6 +350,8 @@ void print_udp(const u_char* packet, pcap_pkthdr packet_header){
  * @param packet_header 
  */
 void print_arp(const u_char* packet, pcap_pkthdr packet_header){
+    fprintf(stderr, "protocol: ARP\n");
+    print_payload(packet,packet_header.len);
 }
 
 /**
@@ -247,9 +360,14 @@ void print_arp(const u_char* packet, pcap_pkthdr packet_header){
  * @param packet 
  * @param packet_header 
  */
-void print_icmp(const u_char* packet, pcap_pkthdr packet_header){
-    print_ip_header(packet);
-    
+void print_icmp(const u_char* packet, pcap_pkthdr packet_header, bool is_ipv6){
+    fprintf(stderr,"protocol: %s\n", (is_ipv6) ? "ICMPv6" : "ICMP");
+    if(!is_ipv6)
+        print_ip_header(packet);
+    else
+        print_ip6_header(packet);
+
+    print_payload(packet,packet_header.len);
 }
 
 int main(int argc, char *argv[])
@@ -347,6 +465,7 @@ int main(int argc, char *argv[])
     int packet_count = 0;
     while (packet_count < n){
         PacketProtocol protocol = Unset;
+        bool is_ipv6 = false;
 
         packet = pcap_next(handle, &packet_header);
         if((packet != NULL) /*&& (&packet_header != NULL)*/){
@@ -372,6 +491,18 @@ int main(int argc, char *argv[])
                     break;
                 }
                 case ETHERTYPE_IPV6:{
+                    {
+                        struct ip6_hdr* ip6_header = (struct ip6_hdr *) (packet + ETH_HLEN);
+                        if(ip6_header->ip6_nxt == IPPROTO_ICMPV6){
+                            protocol = ICMPv6;
+                        }else if(ip6_header->ip6_nxt == IPPROTO_TCP){
+                            protocol = TCP;
+                        }else if(ip6_header->ip6_nxt ==IPPROTO_UDP){
+                            protocol = UDP;
+                        }
+                        is_ipv6 = true;
+                    };
+                    
                     break;
                 }
                 case ETHERTYPE_ARP:{
@@ -387,12 +518,12 @@ int main(int argc, char *argv[])
         if(protocol != Unset){
             print_eth_header(packet,packet_header);
 
-            if(protocol == ICMP)
-                print_icmp(packet,packet_header);
+            if(protocol == ICMP || protocol == ICMPv6)
+                print_icmp(packet,packet_header, is_ipv6);
             else if(protocol == TCP)
-                print_tcp(packet,packet_header);
+                print_tcp(packet,packet_header,is_ipv6);
             else if(protocol == UDP)
-                print_udp(packet,packet_header);
+                print_udp(packet,packet_header,is_ipv6);
             else if(protocol == ARP)
                 print_arp(packet,packet_header);
             packet_count++;
